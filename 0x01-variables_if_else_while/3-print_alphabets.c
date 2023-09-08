5d25c156dd0c01eb99042addd651ded6d2d38d59
GiftUzochi <uzochigift1999@gmail.com>
2023-09-08 14:04:52 -0700

    Print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ *
+ * Return: always 0
+ */
+int main(void)
+{char i;
+
+for (i = 'z' ; i >= 'a' ; i--)
+putchar(i);
+putchar('\n');
+return (0);
+}
