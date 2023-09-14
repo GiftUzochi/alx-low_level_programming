f33a2d97f7a97c6a9d5e6c918c964c43319c8ce5
GiftUzochi <uzochigift1999@gmail.com>
2023-09-14 15:41:54 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,20 +13,16 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		printf("Fizz");
-	}
-	else if (i % 5 == 0 && i % 3 != 0)
-	{
-		printf("Buzz");
-	}
-	else if (i % 3 == 0 && i % 5 == 0)
-	{
-		printf("FizzBuzz");
-	}
-	else
-	{
-		printf(" %d ", i);
-	}
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s", "FizzBuzz");
+			else if (i % 3 == 0)
+			printf("%s", "Fizz");
+			else if (i % 5 == 0)
+			printf("%s", "Buzz");
+			else
+			printf(" %d ", i);
+			if (i != 100)
+			printf(" ");
 }
 printf("\n");
 return (0);
