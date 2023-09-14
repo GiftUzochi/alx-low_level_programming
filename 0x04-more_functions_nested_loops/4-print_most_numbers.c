bcb17d52c275231b1a0f2f1f4c9edd588cffc6f5
GiftUzochi <uzochigift1999@gmail.com>
2023-09-14 12:42:57 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print 0-9
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 0 ; c < 10 ; c++)
+	{
+		if (c != 2 && c != 4)
+
+			_putchar(c + '0');
+	}
+	_putchar('\n');
+}
