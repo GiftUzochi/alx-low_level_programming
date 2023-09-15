9e39d5481cbe9f4f037def040ff73fb9e137db3b
GiftUzochi <uzochigift1999@gmail.com>
2023-09-14 20:00:10 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,25 +2,28 @@
 /**
  * print_triangle - prints a triangle followed by a new line
  * @size: size of the triangle
+ * Return: void
  */
 void print_triangle(int size)
 {
+	int row, hashes, spaces;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		int i, j;
-
-		for (i = 1; i <= size; i++)
-			_putchar(' ');
+		for (row = 1; row <= size; row++)
 		{
-		for (j = i; j <= i; j++)
-		}
-		
-			_putchar('#');
-		{
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				_putchar('#');
+			}
 			_putchar('\n');
 		}
 	}
