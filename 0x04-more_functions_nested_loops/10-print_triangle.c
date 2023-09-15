0c7b017ccc7ab214b2f55cd61d34b32bc120cb04
GiftUzochi <uzochigift1999@gmail.com>
2023-09-14 19:47:41 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,13 +13,14 @@ void print_triangle(int size)
 	{
 		int i, j;
 
-		
-		for (i = 1, i <= size i++);
+		for (i = 1; i <= size; i++)
+			_putchar(' ');
 		{
-			for (j = i; j = size; j++)
-			{
-				_putchar('#');
-			}
+		for (j = i; j <= i; j++)
+		}
+		
+			_putchar('#');
+		{
 			_putchar('\n');
 		}
 	}
