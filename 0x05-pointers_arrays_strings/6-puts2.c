b4ecd2695d6e53bc113e622a686ec34aff0d62bf
GiftUzochi <uzochigift1999@gmail.com>
2023-09-18 15:53:03 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,23 +1,32 @@
 #include "main.h"
 /**
- * rev_string - Reverses a string
- * @s: input
- * Return: string in reverse
+ * puts2 - Prints every other character of a string, starting with the first
+ * @str: input
+ * Description: This function prints every other character of the input string,
+ * starting with the first character. It iterates through the string and prints
+ * the characters at even indices until it reaches the terminating null byte.
+ * Return: print
  */
-void rev_string(char *s)
+void puts2(char *str)
 {
-	char rev = s[0];
-	int counter = 0;
+	int len = 0;
+	int t = 0;
+	char *y = str;
 	int i;
 
-	while (s[counter] != '\0')
-		counter++;
-
-	for (i = 0; i < counter; i++)
+	while (*y != '\0')
 	{
-		counter--;
-		rev = s[i];
-		s[i] = s[counter];
-		s[counter] = rev;
+		y++;
+		len++;
 	}
+	t = len - 1;
+
+		for (i = 0; i <= t; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+	}
+}
+	_putchar('\n');
 }
