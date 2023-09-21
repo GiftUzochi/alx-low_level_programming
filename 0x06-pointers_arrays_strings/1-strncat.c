a745fa52bba709fc43d6eea2dd59ad8404cfbde0
GiftUzochi <uzochigift1999@gmail.com>
2023-09-21 11:01:00 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -20,24 +20,25 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i, j;
+	int i, j;
 
-i = 0;
+	i = 0;
 
-while (dest[i] != '\0')
-{
-i++;
-}
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
-j = 0;
-while (j < n && src[i] != '\0')
-{
-dest[i] = src[j];
-i++;
-j++;
-}
+	j = 0;
 
-dest[i] = '\0';
+	while (j < n && src[i] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-return (dest);
+	dest[i] = '\0';
+
+	return (dest);
 }
