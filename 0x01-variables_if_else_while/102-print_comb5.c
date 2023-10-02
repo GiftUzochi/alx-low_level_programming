9ab9722f0101976bf4f37fc47ea108c7b2f848d1
GiftUzochi <uzochigift1999@gmail.com>
2023-10-02 11:53:03 -0700

    create_static_lib.sh

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,8 +18,8 @@ for (j = 0; j < 100; j++)
 		putchar(' ');
 		putchar((j / 10) + 48);
 		putchar((j % 10) + 48);
-if (i != 98) or (j != 99)
-{
+if (i != 98 || j != 99)
+		{
 			  putchar(',');
 			  putchar(' ');
 }
