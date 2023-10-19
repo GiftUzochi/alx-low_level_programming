5e73242c46078cf879f068784a2e382010e4a1df
GiftUzochi <uzochigift1999@gmail.com>
2023-10-19 15:00:16 -0700

    Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - fxn to print element of list_t
+ * @h: the length of string
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h);
+{
+	while (h != NULL)
+	{
+		if (h->str = NULL)
+			printf([0], (nil)"/n");
+		else
+			printf("[%d]/%s/n", h->str);
+		num_nodes++;
+		h = h->next
+	}
+			return (num_nodes);
+	}
+
+
