01f0e7080f7f4e8237d12e1899b48adc947b5ed0
GiftUzochi <uzochigift1999@gmail.com>
2023-10-20 15:23:07 -0700

    101_first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * print_words - fxn to print sentence before main fxn execution
+ * Return: void
+ */
+
+void print_words(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+		);
+}
