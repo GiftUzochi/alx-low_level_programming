73d0508c77a687a062e9813104db95013cb48bd0
GiftUzochi <uzochigift1999@gmail.com>
2023-10-20 16:12:28 -0700

    add node behind

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_node_end - fxn to add a newnode at the end
+ * @head: the head of the linkedlist
+ * @str: the string to add_node at the end
+ * Return: the address of ptr
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_tail = malloc(sizeof(list_t));
+
+	if (new_tail == NULL)
+	return (NULL);
+	new_tail->str = strdup(str);
+	if (new_tail->str == NULL)
+	{
+		free(new_tail);
+		return (NULL);
+	}
+	new_tail->len = strlen(str);
+	new_tail->next = NULL;
+
+	return (new_tail);
+}
