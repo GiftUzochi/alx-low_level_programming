2e46464e3433d7c927c4e96e3d0bcd2f795d42c4
GiftUzochi <uzochigift1999@gmail.com>
2023-10-20 15:37:35 -0700

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,6 +13,11 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_head == NULL)
 	return (NULL);
 	new_head->str = strdup(str);
+	if (new_head->str == NULL)
+	{
+		free(new_head);
+		return (NULL);
+	}
 	new_head->len = strlen(str);
 	new_head->next = *head;
 
