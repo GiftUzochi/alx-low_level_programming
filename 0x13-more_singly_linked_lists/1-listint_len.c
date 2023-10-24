ab5d7d04d59f00d91fb5729934cd27161d34458c
GiftUzochi <uzochigift1999@gmail.com>
2023-10-24 10:37:40 -0700

    2-print

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,19 +1,18 @@
 #include "lists.h"
 
-/*
- * listint_len - print number of elements in listint_t list
- * @h: the first pointer
- * Return: the num-nodes
+/**
+ * listint_len - search for number of elements in sll
+ * @h: the struct to extract head from
+ * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t listint = 0;
+	size_t num_nodes = 0;
 
 	while (h != NULL)
 	{
-			h = h->next;
-			num-nodes++;
+		num_nodes++;
+		h = h->next;
 	}
-	return (num-nodes);
+	return (num_nodes);
 }
-
