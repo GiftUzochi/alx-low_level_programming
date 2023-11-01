29ed079e7c9f0c66c0e8229a88348c66df8627bd
uzochi <uzochigift1999@gmail.com>
2023-11-01 09:55:12 -0700

    1file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,28 +8,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int desc,
-	    int len, result;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	desc = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-
-	if (desc == -1)
-		return (-1);
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		close(desc);
-		return (1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	len = strlen(text_content);
-		result = write(desc, text_content, len);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
 
-	if (result == -1)
+	if (fd == -1 || w == -1)
 		return (-1);
 
-	close(desc);
+	close(fd);
+
 	return (1);
 }
